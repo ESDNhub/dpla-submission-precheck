ee9527427410d027c88da0235ae41910578cbe3f
jmignault <john.mignault@gmail.com>
2015-02-04 16:53:24 -0500

    harvest display now a variable
    Add a variable to control display of harvest date

--- config.php
+++ config.php
@@ -1,16 +1,17 @@
 <?php
 
 // DPLA Submission Checker Configuration
-        $provider = "ESDN"; // DPLA Service Hub Name
-        $oaibaseurl = "http://repox.metro.org:8080/repox/OAIHandler"; // DPLA Service Hub Base URL
-        $metadataprefix = "mods";  // If this changes, please edit stylesheets in /xsl accordingly
-        $harvestday = "19"; // Day of the month DPLA is scheduled to harvest
-        $helpcontact = "John Mignault"; // Name of Help Contact Person
-        $helpemail = "jmignault@metro.org"; // Email address for Help requests
-        $helpphone = "212 555 1234"; // Phone number for Help requests
-        $TZ = 'America/New_York';
-        
-        // DO NOT EDIT
-        $dataprovider = isset($_GET['dataprovider']) ? $_GET['dataprovider'] : '';
+$provider = "ESDN"; // DPLA Service Hub Name
+$oaibaseurl = "http://repox.metro.org:8080/repox/OAIHandler"; // DPLA Service Hub Base URL
+$metadataprefix = "mods";	 // If this changes, please edit stylesheets in /xsl accordingly
+$harvestday = "19"; // Day of the month DPLA is scheduled to harvest
+$helpcontact = "John Mignault"; // Name of Help Contact Person
+$helpemail = "jmignault@metro.org"; // Email address for Help requests
+$helpphone = "212 555 1234"; // Phone number for Help requests
+$TZ = 'America/New_York';
+$showharvestdate = 0;
+				
+// DO NOT EDIT
+$dataprovider = isset($_GET['dataprovider']) ? $_GET['dataprovider'] : '';
 
 ?>
