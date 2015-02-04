4d7867c194580fc28ea01912cba5401d9dbc8bc6
jmignault <john.mignault@gmail.com>
2015-02-04 16:40:09 -0500

    Move timezone to config
    Make timezone a variable in configuration

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?php
 
 // DPLA Submission Checker Configuration
-        $provider = ""; // DPLA Service Hub Name
-        $oaibaseurl = ""; // DPLA Service Hub Base URL
-        $metadataprefix = "MODS";  // If this changes, please edit stylesheets in /xsl accordingly
+        $provider = "ESDN"; // DPLA Service Hub Name
+        $oaibaseurl = "http://repox.metro.org:8080/repox/OAIHandler"; // DPLA Service Hub Base URL
+        $metadataprefix = "mods";  // If this changes, please edit stylesheets in /xsl accordingly
         $harvestday = "19"; // Day of the month DPLA is scheduled to harvest
-        $helpcontact = ""; // Name of Help Contact Person
-        $helpemail = ""; // Email address for Help requests
-        $helpphone = ""; // Phone number for Help requests
+        $helpcontact = "John Mignault"; // Name of Help Contact Person
+        $helpemail = "jmignault@metro.org"; // Email address for Help requests
+        $helpphone = "212 555 1234"; // Phone number for Help requests
+        $TZ = 'America/New_York';
         
         // DO NOT EDIT
         $dataprovider = isset($_GET['dataprovider']) ? $_GET['dataprovider'] : '';
+
 ?>
