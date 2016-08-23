b70eccf3ee83fcc25e9dc08a8d8514483c5ceda8
jmignault <john.mignault@gmail.com>
2016-08-23 11:37:53 -0400

    add genre data to sample format field; take all format and type

--- samplerecord.php
+++ samplerecord.php
@@ -1,4 +1,4 @@
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
@@ -15,135 +15,135 @@
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+						<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+						<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
     </head>
     <body>                  
         
         <div class="container-fluid">
-        
-          <?php
-          // include application-wide config information
-          include('config.php');
-          
-          // make sure set information is present
-          $set = isset($_GET['set']) ? $_GET['set'] : '';
-          
-          if(empty($set)){
-              // do nothing
-          } else {
-          
+						
+						<?php
+						// include application-wide config information
+						include('config.php');
+						
+						// make sure set information is present
+						$set = isset($_GET['set']) ? $_GET['set'] : '';
+						
+						if(empty($set)){
+								// do nothing
+						} else {
+								
 
-            $listidurl = $oaibaseurl."?verb=ListIdentifiers&set=".$set."&metadataPrefix=".$metadataprefix;
-            $idarray = '';
+								$listidurl = $oaibaseurl."?verb=ListIdentifiers&set=".$set."&metadataPrefix=".$metadataprefix;
+								$idarray = '';
 
-            function getIdList($url) {
-                // establish function to get this set's oai IDs
-                global $idarray;
-                global $oaibaseurl;
+								function getIdList($url) {
+										// establish function to get this set's oai IDs
+										global $idarray;
+										global $oaibaseurl;
 
-                // create curl resource
-                $ch = curl_init();
+										// create curl resource
+										$ch = curl_init();
 
-                // set url
-                curl_setopt($ch, CURLOPT_URL, $url);
+										// set url
+										curl_setopt($ch, CURLOPT_URL, $url);
 
-                //return the transfer as a string
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+										//return the transfer as a string
+										curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-                // $output contains the output string
-                $idoutput = curl_exec($ch);
+										// $output contains the output string
+										$idoutput = curl_exec($ch);
 
-                // close curl resource to free up system resources
-                curl_close($ch);
+										// close curl resource to free up system resources
+										curl_close($ch);
 
 
-                try {
-                    $pagexml = new SimpleXMLElement($idoutput);
-                } catch (Exception $e) {
+										try {
+												$pagexml = new SimpleXMLElement($idoutput);
+										} catch (Exception $e) {
 
-                }
+										}
 
-                if(isset($pagexml->ListIdentifiers)){
+										if(isset($pagexml->ListIdentifiers)){
 
-                    // write all identifiers that do not have status markers to an array
-                    foreach($pagexml->ListIdentifiers->header as $identry){
-                        if(!isset($identry['status'])){
-                            $idarray[] = (string) $identry->identifier;
-                        }
+												// write all identifiers that do not have status markers to an array
+												foreach($pagexml->ListIdentifiers->header as $identry){
+														if(!isset($identry['status'])){
+																$idarray[] = (string) $identry->identifier;
+														}
+
+												}
+
+												// if a resumption token is set loop through the next page of results
+												if (isset($pagexml->ListIdentifiers->resumptionToken)) {
+														$nextlistidurl = $oaibaseurl . "?verb=ListIdentifiers&resumptionToken=" . $pagexml->ListIdentifiers->resumptionToken;
+														getIdList($nextlistidurl);
+												}
 
                     }
+								}
 
-                    // if a resumption token is set loop through the next page of results
-                    if (isset($pagexml->ListIdentifiers->resumptionToken)) {
-                    $nextlistidurl = $oaibaseurl . "?verb=ListIdentifiers&resumptionToken=" . $pagexml->ListIdentifiers->resumptionToken;
-                    getIdList($nextlistidurl);
-                    }
+								getIdList($listidurl);
 
-                    }
-            }
+								// get a random id from the list of ids
+								$idmaxnum = count($idarray)-1;
+								$idrandnum = rand(0,$idmaxnum);
+								$sampleid = $idarray[$idrandnum];
 
-            getIdList($listidurl);
+								// fetch that id's record to output as a sample
+								$sampleurl = $oaibaseurl."?verb=GetRecord&identifier=".$sampleid."&metadataPrefix=".$metadataprefix;
+								// create curl resource
+								$ch = curl_init();
 
-            // get a random id from the list of ids
-            $idmaxnum = count($idarray)-1;
-            $idrandnum = rand(0,$idmaxnum);
-            $sampleid = $idarray[$idrandnum];
+								// set url
+								curl_setopt($ch, CURLOPT_URL, $sampleurl);
 
-            // fetch that id's record to output as a sample
-            $sampleurl = $oaibaseurl."?verb=GetRecord&identifier=".$sampleid."&metadataPrefix=".$metadataprefix;
-             // create curl resource
-            $ch = curl_init();
+								//return the transfer as a string
+								curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-            // set url
-            curl_setopt($ch, CURLOPT_URL, $sampleurl);
+								// $output contains the output string
+								$sampleoutput = curl_exec($ch);
 
-            //return the transfer as a string
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+								// close curl resource to free up system resources
+								curl_close($ch);
 
-            // $output contains the output string
-            $sampleoutput = curl_exec($ch);
+								try {
+										$samplexml = new SimpleXMLElement($sampleoutput);
+								} catch (Exception $e) {
+								}
 
-            // close curl resource to free up system resources
-            curl_close($ch);
+								// transform the sample record to make data easier to address
+								$samplexsl = new DOMDocument;
+								$samplexslpath = 'xsl/samplerecord.xsl';
+								$samplexsl->load($samplexslpath);
+								$sampleproc = new XSLTProcessor;
+								$sampleproc->importStylesheet($samplexsl);
 
-            try {
-                $samplexml = new SimpleXMLElement($sampleoutput);
-            } catch (Exception $e) {
-            }
+								$sampleresult = trim($sampleproc->transformToXML($samplexml));
 
-            // transform the sample record to make data easier to address
-            $samplexsl = new DOMDocument;
-            $samplexslpath = 'xsl/samplerecord.xsl';
-            $samplexsl->load($samplexslpath);
-            $sampleproc = new XSLTProcessor;
-            $sampleproc->importStylesheet($samplexsl);
+								$samplearray= simplexml_load_string($sampleresult);
 
-            $sampleresult = trim($sampleproc->transformToXML($samplexml));
+								// vars with one value
+								$oai_id = isset($samplearray->oai_id) ? $samplearray->oai_id : '';
+								$title = isset($samplearray->title) ? $samplearray->title : '';
+								$url = isset($samplearray->url) ? $samplearray->url : '';
+								$thumburl = !empty($samplearray->thumburl) ? $samplearray->thumburl : 'img/thumbnail.png';
+								$rights = isset($samplearray->rights) ? $samplearray->rights : '';
+								$description = isset($samplearray->description) ? $samplearray->description : '';
+								$contributing_institution = isset($samplearray->contributing_institution) ? $samplearray->contributing_institution : '';
 
-            $samplearray= simplexml_load_string($sampleresult);
-
-            // vars with one value
-            $oai_id = isset($samplearray->oai_id) ? $samplearray->oai_id : '';
-            $title = isset($samplearray->title) ? $samplearray->title : '';
-            $url = isset($samplearray->url) ? $samplearray->url : '';
-            $thumburl = !empty($samplearray->thumburl) ? $samplearray->thumburl : 'img/thumbnail.png';
-            $rights = isset($samplearray->rights) ? $samplearray->rights : '';
-            $format = isset($samplearray->format) ? $samplearray->format : '';
-            $type = isset($samplearray->type) ? $samplearray->type : '';
-            $description = isset($samplearray->description) ? $samplearray->description : '';
-            $contributing_institution = isset($samplearray->contributing_institution) ? $samplearray->contributing_institution : '';
-
-            // vars with multiple values, potentially
-            $creator = isset($samplearray->creator->data) ? $samplearray->creator->data : array();
-            $date = isset($samplearray->date->data) ? $samplearray->date->data : array();
-            $publisher = isset($samplearray->publisher->data) ? $samplearray->publisher->data : array();
-            $location = isset($samplearray->location->data) ? $samplearray->location->data : array();
-            $subject = isset($samplearray->subject->data) ? $samplearray->subject->data : array();
-              $language = isset($samplearray->language->data) ? $samplearray->language->data : array();
-           
-          ?>
+								// vars with multiple values, potentially
+								$creator = isset($samplearray->creator->data) ? $samplearray->creator->data : array();
+								$date = isset($samplearray->date->data) ? $samplearray->date->data : array();
+								$publisher = isset($samplearray->publisher->data) ? $samplearray->publisher->data : array();
+								$location = isset($samplearray->location->data) ? $samplearray->location->data : array();
+								$subject = isset($samplearray->subject->data) ? $samplearray->subject->data : array();
+								$language = isset($samplearray->language->data) ? $samplearray->language->data : array();
+								$format = isset($samplearray->format->data) ? $samplearray->format->data : array();
+								$type = isset($samplearray->type->data) ? $samplearray->type->data : array();
+								
+						?>
             <div class="row">
                 <div class="col-md-6"><h2>Sample Record 
                     <span class="small">
@@ -170,74 +170,73 @@
                                                 <tr><th>Creator</th><td><?php foreach( $creator as $singlecreator) { echo $singlecreator."<br/>"; };?></td></tr>
 
                                             <?php } ?>
-                                                        
+                                            
                                             <?php if(!empty($date)) { ?>
                                                 <!--<tr><th>Created Date</th><td><?php foreach( $date as $singledate) { echo $singledate."<br/>"; };?></td></tr>-->
                                                 <tr><th>Created Date  <a class="helpinfo text-muted" data-toggle="popover" data-content="The DPLA will attempt to normalize dates as records are harvested.">
                                                     <span class="glyphicon glyphicon-question-sign"></span>
                                                 </a></th><td><?php foreach( $date as $singledate) { echo $singledate."<br/>"; };?></td></tr>
                                                 <?php } ?>
-                                                        
+                                                
                                                 <?php if($provider!=='') { ?>
                                                     <tr><th>Partner</th><td><?php echo $provider;?></td></tr>
                                                 <?php } ?>
-                                                        
+                                                
                                                 <?php if((string) $contributing_institution!=='') { ?>
                                                     <tr><th>Contributing Institution</th><td><?php echo $contributing_institution;?></td></tr>
                                                 <?php } ?>
-                                                        
+                                                
                                                 <?php if(!empty($publisher)) { ?>
                                                     <tr><th>Publisher</th><td><?php foreach( $publisher as $singlepub) { echo $singlepub."<br/>"; };?></td></tr>
                                                 <?php } ?>
-                                                        
+                                                
                                                 <?php if((string) $description!=='') { ?>
                                                     <tr><th>Description</th><td><?php echo $description;?></td></tr>
                                                 <?php } ?>
-                                                        
+                                                
                                                 <?php if(!empty($location)) { ?>
                                                     <tr><th>Location  <a class="helpinfo text-muted" data-toggle="popover" data-content="The DPLA will attempt to normalize/geocode locations as records are harvested.">
-							<span class="glyphicon glyphicon-question-sign"></span>
-						    </a></th><td><?php foreach( $location as $singleloc) { echo $singleloc."<br/>"; };?></td></tr>
+																												<span class="glyphicon glyphicon-question-sign"></span>
+																										</a></th><td><?php foreach( $location as $singleloc) { echo $singleloc."<br/>"; };?></td></tr>
                                                 <?php } ?>
-                                                        
-                                                <?php if((string) $format!=='') { ?>
-                                                    <tr><th>Format</th><td><?php echo $format;?></td></tr>
+                                                
+                                                <?php if(!empty($format)) { ?>
+                                                    <tr><th>Format</th><td><?php foreach( $format as $singlefmt) { echo $singlefmt."<br/>"; };?></td></tr>
                                                 <?php } ?>
-                                                        
-                                                        
-                                                <?php if((string) $type!=='') { ?>
-                                                    <tr><th>Type</th><td><?php echo $type;?></td></tr>
+                                                
+                                                <?php if(!empty($type)) { ?>
+                                                    <tr><th>Type</th><td><?php foreach( $type as $singletype) { echo $singletype."<br/>"; };?></td></tr>
                                                 <?php } ?>
-                                                        
+                                                
                                                 <?php if(!empty($subject)) { ?>
                                                     <tr><th>Subject</th><td><?php foreach( $subject as $singlesub) { echo $singlesub."<br/>"; };?></td></tr>
                                                 <?php } ?>
-                                                        
+                                                
                                                 <?php if(!empty($language)) { ?>
                                                     <tr><th>Language <a class="helpinfo text-muted" data-toggle="popover" data-content="The DPLA will attempt to normalize language terms as records are harvested.">
-							<span class="glyphicon glyphicon-question-sign"></span>
-						    </a></th><td><?php foreach( $language as $singlelang) { echo $singlelang."<br/>"; };?></td></tr>
+																												<span class="glyphicon glyphicon-question-sign"></span>
+																										</a></th><td><?php foreach( $language as $singlelang) { echo $singlelang."<br/>"; };?></td></tr>
                                                 <?php } ?>
                                                 
                                                 <?php if((string) $rights!=='') { ?>
                                                     <tr><th>Rights</th><td><?php echo $rights;?></td></tr>
                                                 <?php } ?>
-                                                        
+                                                
                                                 <?php if((string) $url!=='') { ?>
                                                     <tr><th>URL</th><td><a target="_blank" href="<?php echo $url;?>"><?php echo $url;?></a> 
-							<a class="oailink" target="_blank" href="viewoai.php?identifier=<?php echo $sampleid;?>&set=<?php echo $set;?>"><span class="small text-muted glyphicon glyphicon-eye-open"></span></a>
+																												<a class="oailink" target="_blank" href="viewoai.php?identifier=<?php echo $sampleid;?>&set=<?php echo $set;?>"><span class="small text-muted glyphicon glyphicon-eye-open"></span></a>
                                                     </td></tr>
                                                 <?php } ?>
                                         </tbody>
                                     </table>
                                 </div>
-                                          
+                                
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-					    <?php    } ?>
+																						<?php    } ?>
         </div>
         <br/>
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
